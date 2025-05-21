4b6f68f34c785a4158e04fbbbdc5d50a80f75a19
muhammad mahrus zain <mahrus@pcr.ac.id>
2025-05-21 15:42:17 +0700

    saya sudah selesai buat produk crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes,Route } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
+import ProductManagement from "./pages/ProductManagement";
 
 export default function App(){
   return(
     <Routes>
       <Route element={<MainLayout/>}>
       <Route path="/" element={<Dashboard/>}></Route>
+      <Route path="produk" element={<ProductManagement/>}></Route>
       </Route>
      
     </Routes>
